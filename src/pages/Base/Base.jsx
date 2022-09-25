b33070ef3f0706a7429be0e6dd6e21deaf283527
topul <qiaocuixinzhiweini@gmail.com>
2022-09-25 15:18:31 +0800

    feat: 完善工具

--- src/pages/Base/Base.jsx
+++ src/pages/Base/Base.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import { Radio, Input } from 'antd'
 
 const Base = () => {
-  return (
-    <div>Base
+  const [fromRadix, setFromRadix] = useState(10)
+  const [src, setSrc] = useState(100)
+  const [toRadix, setToRadix] = useState(16)
+  const [destValue, setDestValue] = useState()
+
+  const transfer = () => {
+    setDestValue(parseInt(src, fromRadix).toString(toRadix))
+  }
+  useEffect(() => {
+    transfer()
+  }, [fromRadix, src, toRadix])
+  const onChange = (e) => {
+    console.log(e)
+    setFromRadix(e.target.value)
+  }
+  const srcChange = (e) => {
+    setSrc(e.target.value)
+  }
+  const destChange = (e) => {
+    setToRadix(e.target.value)
+  }
+
+  return <div>
+    <div>
+      <Radio.Group onChange={onChange} value={fromRadix}>
+        <Radio value={2}>2进制</Radio>
+        <Radio value={8}>8进制</Radio>
+        <Radio value={10}>10进制</Radio>
+        <Radio value={16}>16进制</Radio>
+      </Radio.Group>
+      <div>
+      原始数字：<Input placeholder="原始数字" value={src} onChange={srcChange} />
+      </div>
+      <Radio.Group onChange={destChange} value={toRadix}>
+        <Radio value={2}>2进制</Radio>
+        <Radio value={8}>8进制</Radio>
+        <Radio value={10}>10进制</Radio>
+        <Radio value={16}>16进制</Radio>
+      </Radio.Group>
+      <div>
+        结果：<Input placeholder="结果" value={destValue} />
+      </div>
     </div>
-  )
+  </div>
 }
 export default Base
--- src/pages/temp/Temp.jsx
+++ src/pages/temp/Temp.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { Input } from 'antd'
+import './temp.css'
 
 const Temp = () => {
+  const [c, setC] = useState(30)
+  const [f, setF] = useState()
+  useEffect(() => {
+    setF(centigradeToFahrenheit(c))
+  }, [])
+  /**
+   * 华氏度转摄氏度
+   */
+  const fahrenheitToCentigrade = (fahrenheit) => {
+    if (Number.isNaN(fahrenheit)) {
+      throw new Error('not a valid number')
+    }
+    return ((fahrenheit - 32) / 1.8).toFixed(2)
+  }
+
+  /**
+   * 摄氏度转华氏度
+   */
+  const centigradeToFahrenheit = (centigrade) => {
+    if (Number.isNaN(centigrade)) {
+      throw new Error('not a valid number')
+    }
+    return (centigrade * 1.8 + 32).toFixed(2)
+  }
+
+  const onCChange = (e) => {
+    setC(e.target.value)
+    setF(centigradeToFahrenheit(e.target.value))
+  }
+
+  const onFChange = (e) => {
+    setF(e.target.value)
+    setC(fahrenheitToCentigrade(e.target.value))
+  }
+
   return (
     <div>
-      Temp
+      <div>
+        <div>
+          摄氏度（℃）：
+          <Input
+            placeholder='请输入摄氏度'
+            value={c}
+            onChange={onCChange}
+          />
+        </div>
+        <div>
+          华氏度（℉）：
+          <Input
+            placeholder='请输入摄氏度'
+            value={f}
+            onChange={onFChange}
+          />
+        </div>
+        <div className='comment'>
+          计算公式：华氏度 = 摄氏度 * 1.8 + 32
+        </div>
+      </div>
     </div>
   )
 }
